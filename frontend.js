abcf596fc2a487c59322ea31edcbb16bb99cbc37
ShirleyXW <shirley.wang.xl@outlook.com>
2025-02-05 12:10:22 -0800

    initial class and object constructor

--- frontend.js
+++ frontend.js
@@ -1,6 +1,41 @@
+"use strict";
 const xhttp = new XMLHttpRequest();
-function fetch(){
-        console.log("fetching");
+
+function Word(word, definition){
+    this.word = word;
+    this.definition = definition;
+}
+
+class Dictionary{
+    constructor(){
+        this.words = {};
+    }
+    addWord(word){
+        this.words[word.word] = word.definition;
+    }
+    getWord(word){
+        return this.words[word];
+    }
+}
+const dictionary = new Dictionary();
+
+function isValidInput(){
+
+    const word = document.getElementById("word").value;
+    const definition = document.getElementById("definition").value;
+    if(word == "" || definition == ""){
+        alert("Please fill in all fields");
+        return false;
+    }
+    return true;
+}
+
+function store(){
+        const wordName = document.getElementById("word").value;
+        const definition = document.getElementById("definition").value;
+        const word = new Word(wordName, definition);
+        dictionary.addWord(word);
+        console.log(dictionary.words);
         xhttp.open("GET", "http://localhost:8000/", true);
         xhttp.send();
         xhttp.onreadystatechange = function(){
